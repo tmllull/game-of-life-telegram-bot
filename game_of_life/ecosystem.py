5ba1c1db1dabf4fd41cad1d2a3ed779738f352cb
Toni Miquel <tonimiquel.llull@gmail.com>
2024-04-11 10:08:02 +0200

    Add 'trophic levels', and new variant that an organism can be devored by anothers

--- game_of_life/ecosystem.py
+++ game_of_life/ecosystem.py
@@ -13,6 +13,9 @@ config = Config()
 class Ecosystem:
 
     def __init__(self) -> None:
+        """
+        Initialize the object.
+        """
         pass
 
     def new_ecosystem(self, seed=None):
@@ -22,8 +25,8 @@ class Ecosystem:
             random.seed(seed)
         rows = config.ROWS
         columns = config.COLUMNS
-        flora_list = flora.get_flora()
-        fauna_list = fauna.get_fauna()
+        flora_list = flora.get_organisms()
+        fauna_list = fauna.get_organisms()
         ecosystem = [[" " for _ in range(columns)] for _ in range(rows)]
 
         # Puts up to three flora organisms
@@ -69,6 +72,7 @@ class Ecosystem:
             for j in range(columns):
                 # Count the number of neighbors that are alive
                 neighbors_alive = 0
+                neighbors = []
                 for dx in [-1, 0, 1]:
                     for dy in [-1, 0, 1]:
                         if dx == 0 and dy == 0:
@@ -76,21 +80,27 @@ class Ecosystem:
                         x = (i + dx) % rows
                         y = (j + dy) % columns
                         if (
-                            ecosystem[x][y] in flora.get_flora()
-                            or ecosystem[x][y] in fauna.get_fauna()
+                            ecosystem[x][y] in flora.get_organisms()
+                            or ecosystem[x][y] in fauna.get_organisms()
                         ):
                             neighbors_alive += 1
+                            neighbors.append(ecosystem[x][y])
 
                 if ecosystem[i][j] != " ":  # Alive cell. Check if it should die
                     if neighbors_alive < 2 or neighbors_alive > 3:
+                        logger.info(
+                            "Organism "
+                            + ecosystem[i][j]
+                            + " died because of superpopulation or infrapopulation"
+                        )
                         new_generation[i][j] = " "  # Die
                     else:
                         new_generation[i][j] = organisms.evolution(
-                            ecosystem[i][j], evolutions
+                            ecosystem[i][j], evolutions, neighbors
                         )  # Stay (if not die on evolution)
                 else:  # Empty cell. Check if it should be born
                     if neighbors_alive == 3:  # New organism born
-                        all_organisms = flora.get_flora() + fauna.get_fauna()
+                        all_organisms = flora.get_organisms() + fauna.get_organisms()
                         organism = random.choice(all_organisms)
                         new_generation[i][j] = organism
                     else:  # Stay empty
--- game_of_life/organisms.py
+++ game_of_life/organisms.py
@@ -6,28 +6,190 @@ from utils.config import Config
 config = Config()
 
 
+import random
+
+
 class Organisms:
     def __init__(self):
         pass
 
-    def evolution(self, organism, evolutions):
+    def evolution(self, organism, evolutions, neighbors):
         # Probability that organisms die
         current_probability_to_die = config.ORG_PROB_DIE * evolutions
-        # logger.info("Organism probability to die: " + str(current_probability_to_die))
+        # Check if organisms is devored by neighbors
+        if self.is_devoured(organism, neighbors):
+            logger.info("Organism " + organism + " devoured")
+            return " "
         if random.random() < current_probability_to_die:
+            logger.info("Organism " + organism + " died accidentally")
             return " "
         return organism
 
+    def get_organism_type(self, organism):
+        if organism in self.Flora().get_organisms():
+            return self.Flora
+        if organism in self.Fauna().TrophicLevel2().get_organisms():
+            return self.Fauna.TrophicLevel2
+        if organism in self.Fauna.TrophicLevel3().get_organisms():
+            return self.Fauna.TrophicLevel3
+        if organism in self.Fauna.TrophicLevel4().get_organisms():
+            return self.Fauna.TrophicLevel4
+
+    def get_survival(self, organism):
+        if organism in self.Flora().get_organisms():
+            return self.Flora().survival
+        if organism in self.Fauna().TrophicLevel1().get_organisms():
+            return self.Fauna.TrophicLevel1().survival
+        if organism in self.Fauna().TrophicLevel2().get_organisms():
+            return self.Fauna.TrophicLevel2().survival
+        if organism in self.Fauna.TrophicLevel3().get_organisms():
+            return self.Fauna.TrophicLevel3().survival
+        if organism in self.Fauna.TrophicLevel4().get_organisms():
+            return self.Fauna.TrophicLevel4().survival
+
+    def is_devoured(self, organism, neighbors):
+        organism_survival = self.get_survival(organism)
+        neighbors_survival = 0
+        for neighbor in neighbors:
+            if organism_survival <= self.get_survival(neighbor):
+                neighbors_survival += self.get_survival(neighbor)
+        # logger.info(
+        #     organism
+        #     + " ("
+        #     + str(organism_survival * len(neighbors))
+        #     + ")"
+        #     + " vs "
+        #     + str(neighbors)
+        #     + " ("
+        #     + str(neighbors_survival)
+        #     + ")"
+        # )
+        if organism_survival * len(neighbors) < neighbors_survival:
+            return True
+        return False
+
     class Flora:
         def __init__(self):
-            pass
+            self.survival = 1
+            self.organisms = [
+                "🌱",
+                "🌳",
+                "🌻",
+                "🍄",
+                "🌹",
+                "🍀",
+                "🌺",
+                "🌷",
+                "🌸",
+                "🌼",
+                "🌾",
+                "🎍",
+                "🌴",
+                "🌵",
+                "🌲",
+                "🌰",
+            ]
 
-        def get_flora(self):
-            return ["🌱", "🌳", "🌻", "🍄", "🌹", "🍀"]
+        def get_organisms(self):
+            return self.organisms
 
     class Fauna:
         def __init__(self):
             pass
 
-        def get_fauna(self):
-            return ["🐇", "🐦", "🐍", "🦠", "🐜", "🦋"]
+        def get_organisms(self):
+            return (
+                self.TrophicLevel1().get_organisms()
+                + self.TrophicLevel2().get_organisms()
+                + self.TrophicLevel3().get_organisms()
+                + self.TrophicLevel4().get_organisms()
+            )
+
+        class TrophicLevel1:
+            def __init__(self):
+                super().__init__()
+                self.survival = 1
+                self.organisms = [
+                    "🐞",
+                    "🐛",
+                    "🦗",
+                    "🐜",
+                    "🦟",
+                    "🐝",
+                    "🦋",
+                    "🪰",
+                    "🐌",
+                    "🪲",
+                ]
+
+            def get_organisms(self):
+                return self.organisms
+
+        class TrophicLevel2:
+            def __init__(self):
+                # super().__init__()
+                self.survival = 2
+                self.organisms = [
+                    "🐰",
+                    "🦌",
+                    "🐄",
+                    "🐿️",
+                    "🦡",
+                    "🦔",
+                    "🦙",
+                    "🐫",
+                    "🦘",
+                    "🐏",
+                    "🐐",
+                    "🐦",
+                    "🦜",
+                    "🐤",
+                    "🐦",
+                ]
+
+            def get_organisms(self):
+                return self.organisms
+
+        class TrophicLevel3:
+            def __init__(self):
+                # super().__init__()
+                self.survival = 3
+                self.organisms = [
+                    "🐷",
+                    "🐒",
+                    "🦝",
+                    "🐴",
+                    "🐖",
+                    "🦓",
+                    "🦒",
+                    "🦥",
+                    "🦦",
+                    "🦊",
+                    "🐘",
+                ]
+
+            def get_organisms(self):
+                return self.organisms
+
+        class TrophicLevel4:
+            def __init__(self):
+                # super().__init__()
+                self.survival = 4
+                self.organisms = [
+                    "🐺",
+                    "🦁",
+                    "🐅",
+                    "🐆",
+                    "🐊",
+                    "🐻",
+                    "🐯",
+                    "🐍",
+                    "🦍",
+                    "🐗",
+                    "🦛",
+                    "🐃",
+                    "🦏",
+                ]
+
+            def get_organisms(self):
+                return self.organisms
